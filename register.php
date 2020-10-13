cc2738a4989295b31882309d06bf33270b536a86
hemant krishna <you@example.com>
2020-10-13 14:44:09 +0530

    added register php

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php
+$dbhost = 'localhost';
+ $dbuser = 'root'; 
+$dbpass = ''; 
+$Fname = $_POST['Fname'];
+$Job = $_POST['Job'];
+$Company = $_POST[ 'Company'];
+$Email =$_POST[ 'Email'];
+$Phone=$_POST[ 'Phone'];
+$Industry = $_POST['Industry'];
+$Country = $_POST[ 'Country'];
+
+
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+ $found=mysqli_select_db( $conn,'blockchain' );
+if(! $found)
+die('Could not find database: ' . mysqli_error()); 
+
+$sql = "INSERT INTO register values ('$Fname','$Job','$Company','$Email','$Phone','$Industry','$Country')";
+
+$retval = mysqli_query( $conn,$sql ); 
+if(! $retval )
+ echo "<h1> unsuccefull".mysqli_error($conn);
+else
+{
+ header("location: registration_form.html");
+
+}
+?>
